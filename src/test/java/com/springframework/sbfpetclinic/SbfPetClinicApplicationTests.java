43e30ab56d62410ff1312bdde6620c2705b75e56
Georgi <chishi@abv.bg>
2019-10-06 20:35:39 +0300

    Initial git commit

---
+++ src/test/java/com/springframework/sbfpetclinic/SbfPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springframework.sbfpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbfPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
